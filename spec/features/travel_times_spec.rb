36275e709f75ab8a9e2a44f3a31597ee3d9f1ea3
Tacuma Bell <takumab@gmail.com>
2018-02-19 14:22:32 +0000

    Added failing feature test for form

--- spec/features/travel_times_spec.rb
+++ spec/features/travel_times_spec.rb
@@ -9,8 +9,17 @@ RSpec.feature "Travel Times", type: :feature do
     end
   end
 
-  # context "input travel input" do
-  #
-  # end
+  context "Get predicted travel time" do
+    scenario "should be successful" do
+      visit index_path
+      within('form') do
+        fill_in 'Start', with: '51.5201, 0.0933'
+        fill_in 'End', with: '51.5461,0.0742'
+        fill_in 'Arrival', with: '2018-02-06T13:00:02-0500'
+      end
+      click_button 'Set'
+      expect(page).to have_content('Travel time: 29 minutes')
+    end
+  end
 
 end
