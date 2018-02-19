bdcc14895536cf48929cb8e96170618ad5d43004
Tacuma Bell <takumab@gmail.com>
2018-02-19 14:04:24 +0000

    Minor change to context sentence

--- spec/features/travel_times_spec.rb
+++ spec/features/travel_times_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Travel Times", type: :feature do
 
   context "title is TravelTimeApp" do
-    scenario "user should see Travel Time App in title" do
+    scenario "user should see TravelTimeApp in title" do
       visit index_path
       expect(title).to have_content 'TravelTimeApp'
     end
