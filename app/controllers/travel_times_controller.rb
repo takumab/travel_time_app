666fcbed3b78d8fee56ac1e3b928b8addf909458
Tacuma Bell <takumab@gmail.com>
2018-02-21 20:48:36 +0000

    Simplified variable name and added instance creation of citymapper object

--- app/controllers/travel_times_controller.rb
+++ app/controllers/travel_times_controller.rb
@@ -1,6 +1,8 @@
 class TravelTimesController < ApplicationController
 
+
   def index
-    
+    @cm = CityMapperApi.new(params[:start], params[:end], params[:time_and_date], params[:arrival])
+    # CityMapperApi.new('51.5201,0.0933', '51.5461,0.0742', '2018-02-21T19:00:02-0500', 'arrival')
   end
 end
