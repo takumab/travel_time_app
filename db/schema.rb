ed37fec793ee587ee7fbec5ee905de182ddcc9b9
Tacuma Bell <takumab@gmail.com>
2018-02-25 14:04:43 +0000

    Added Location model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180224153255) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "start_address"
+    t.string "end_address"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
