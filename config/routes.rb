63736fd0759a68d5fc2ba959f60cb07519c91726
Tacuma Bell <takumab@gmail.com>
2018-02-25 14:03:56 +0000

    Added routes for locaitions and changed the root path to locations form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :locations
+  root 'locations#new'
   get 'index', to: 'travel_times#index', as: :index
 end
