373219fa72f37842eb07b9ed2895506f6be460f4
Tacuma Bell <takumab@gmail.com>
2018-02-27 19:40:45 +0000

    Added geocoder to destination and conditions to both models

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ApplicationRecord
   geocoded_by :start_address
-  after_validation :geocode
+  after_validation :geocode, if: :address_changed?
 
-  has_one :destination 
+  has_one :destination
 end
