3647b5767ef7e00b75a64fa2d2df2fb6f1db2dd8
Tacuma Bell <takumab@gmail.com>
2018-02-28 20:14:53 +0000

    Removed inverse_of

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,6 @@ class Location < ApplicationRecord
   geocoded_by :start_address
   after_validation :geocode, if: :start_address_changed?
 
-  has_one :destination, inverse_of: :location
+  has_one :destination 
   accepts_nested_attributes_for :destination
 end
