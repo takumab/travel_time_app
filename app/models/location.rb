0d1f9f375bb5fc96e6b10fcec95b40fa31ebc17d
Tacuma Bell <takumab@gmail.com>
2018-03-01 13:57:57 +0000

    Added bootstrap and class method (loc_coords) on location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,16 @@ class Location < ApplicationRecord
   geocoded_by :start_address
   after_validation :geocode, if: :start_address_changed?
 
-  has_one :destination 
+  has_one :destination
   accepts_nested_attributes_for :destination
+
+
+  def self.loc_coords
+    latitude = Location.pluck(:latitude)
+    longitude = Location.pluck(:longitude)
+    lat_long = latitude.zip(longitude)
+    lat_long.delete_if { |array| array.include?('0.0') }
+    lat_long.delete_if { |array| array.include?(nil) }
+  end
+
 end
