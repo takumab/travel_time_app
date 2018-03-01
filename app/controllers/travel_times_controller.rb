d41d89c6ca2bbe9ae7ecdf3e677dd312fa2a814a
Tacuma Bell <takumab@gmail.com>
2018-03-01 21:46:34 +0000

    Tidy up folders

--- app/controllers/travel_times_controller.rb
+++
@@ -1,9 +0,0 @@
-class TravelTimesController < ApplicationController
-
-  # STARTING_PLACE = Barbican
-  # if params[:params].blank? ....
-  def index
-    # CityMapperApi.new('51.5201,0.0933', '51.5461,0.0742', '2018-02-21T19:00:02-0500', 'arrival')
-    @cm = CityMapperApi.new(params[:start], params[:end], params[:time_and_date], params[:arrival])
-  end
-end
--- app/models/location.rb
+++ app/models/location.rb
@@ -5,13 +5,4 @@ class Location < ApplicationRecord
   has_one :destination
   accepts_nested_attributes_for :destination
 
-
-  def self.loc_coords
-    latitude = Location.pluck(:latitude)
-    longitude = Location.pluck(:longitude)
-    lat_long = latitude.zip(longitude)
-    lat_long.delete_if { |array| array.include?('0.0') }
-    lat_long.delete_if { |array| array.include?(nil) }
-  end
-
 end
